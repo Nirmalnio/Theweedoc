2fc8b426b26cfdf9869e27d5d31a2714ddb77bb3
Nirmalnio <nirmalnio123@gmail.com>
2023-06-03 12:31:22 +0530

    first commit

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main style={{display:"flex",justifyContent:"center"}}>
+           <h1>THEWEEDOC</h1>
+    </main>
+  )
+}
